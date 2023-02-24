62b8009f0073d53708e1f35a281e8616b9252ad9
Dust <58159486+ls2658110655@users.noreply.github.com>
2023-02-24 09:52:13 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 
 int main()
 {
+	aaccodec aac;
+	aac.aacencodec("test/111.pcm", "test/111.aac");
+//	aac.aacdecodec("test/111.aac", "test/111.pcm");
 	h264codec h264;
 //	h264.h264encodec("test.yuv", "test.h264");
-	h264.h264decedec("111.h264", "test/111.yuv");
+//	h264.h264decedec("111.h264", "test/111.yuv");
 
 //	cout << "HelloFFmpeg" << endl;
 //	printf("%s", avcodec_configuration());
